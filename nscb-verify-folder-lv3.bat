d9a1c977a0476137e81774cb9fde33fd83e5e3b0
seiya-git <seiya.loveless.mail@gmail.com>
2023-09-15 14:38:58 +0300

    add extra bat files

---
+++ nscb-verify-folder-lv3.bat
@@ -0,0 +1,8 @@
+@echo off
+set squirrel=%~dp0/py/ztools/squirrel.py
+
+for %%a in ("*.xci", "*.xcz", "*.nsp", "*.nsz") do (
+    py "%squirrel%" -v "%%~dpnxa" -vt lv3 -o "%%~dpa." --saveverifylog 1
+)
+
+pause
---
+++ nscb-verify-lv3.bat
@@ -0,0 +1,6 @@
+@echo off
+set squirrel=%~dp0/py/ztools/squirrel.py
+
+py "%squirrel%" -v "%~1" -vt lv3 -o "%~dp1." --saveverifylog 1
+
+pause
