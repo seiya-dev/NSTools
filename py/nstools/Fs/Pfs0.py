ee8771320261a19b6a13857723302f6713fbf1a8
seiya-git <seiya.loveless.mail@gmail.com>
2024-01-01 12:21:20 +0300

    PFS0 fixes

--- py/nstools/Fs/Pfs0.py
+++ py/nstools/Fs/Pfs0.py
@@ -160,7 +160,7 @@ class Pfs0VerifyStream():
 			self._stringTableSize = stringTableSizePadded
 		elif len(stringTableNonPadded) > self._stringTableSize:
 			self._stringTableSize = len(stringTableNonPadded)
-		return self._stringTableSize-1
+		return self._stringTableSize
 	
 	def getHash(self):
 		hexHash = self.binhash.hexdigest()
@@ -188,7 +188,7 @@ class Pfs0VerifyStream():
 		
 		if len(self.files) > 0:
 			if self.files[0]['offset'] - headerSize > 0:
-				stringTable += '\x00' * (self.files[0]['offset'] - headerSize - 1)
+				stringTable += '\x00' * (self.files[0]['offset'] - headerSize)
 		h += stringTable.encode()
 		
 		headerHex = h.hex()
